269ee8f4badac4114f102e94de7e315d597245bb
Lars Duelfer <lars.duelfer@novatec-gmbh.de>
2021-02-04 14:35:32 +0100

    Spring boot showcase to report issue with sleuth and spring cloud gateway

---
+++ gateway/src/main/java/com/example/gateway/GatewayApplication.java
@@ -0,0 +1,19 @@
+package com.example.gateway;
+
+import brave.sampler.Sampler;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class GatewayApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(GatewayApplication.class, args);
+  }
+
+  @Bean
+  public Sampler defaultSampler() {
+    return Sampler.ALWAYS_SAMPLE;
+  }
+}
---
+++ service/src/main/java/com/example/backend/BackendApplication.java
@@ -0,0 +1,19 @@
+package com.example.backend;
+
+import brave.sampler.Sampler;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class BackendApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(BackendApplication.class, args);
+  }
+
+  @Bean
+  public Sampler defaultSampler() {
+    return Sampler.ALWAYS_SAMPLE;
+  }
+}
